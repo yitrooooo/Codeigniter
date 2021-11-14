eb18cbca25f5e1f1456d61a2054aa03f7bee8d91
yitrooooo <91476758+yitrooooo@users.noreply.github.com>
2021-11-15 02:48:24 +0700

    Create contoh1.php

---
+++ contoh1.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title>Contoh 1</title>
+</head>
+<body>
+    <form action="contoh2.php" method="post">
+    Nama :  <input type = "text" name="nama" size="25"><br>
+    Kelas:  <input type = "text" name="kelas" size="25"><br>
+    <input type="submit" value="Input">
+</form>
+</body>
+</html> 
